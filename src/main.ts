41c31df982a2d616991b8930cb22d515209a2277
Gabriel Barker <gabrielbarker@gmail.com>
2020-05-21 16:09:41 +0100

    ADD an html file and a main window handler

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,10 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
     },
     width: 800,
+    titleBarStyle: "hidden",
   });
 
   // and load the index.html of the app.
