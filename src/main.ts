90b938be5a0ff57f0c457cf9682390dd12625cae
Gabriel Barker <gabrielbarker@gmail.com>
2020-06-02 17:57:04 +0100

    EXTEND the app vertically

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ let mainWindow: Electron.BrowserWindow | null;
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    height: 770,
+    height: 780,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
